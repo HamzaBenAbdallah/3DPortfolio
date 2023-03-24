6a17203167f923fe86b0027ce41f685fb74f5b4f
Hamza <hamza.ben.abdallah02@gmail.com>
2023-03-24 00:45:05 -0400

    Remove the tilt from the cards

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -7,66 +7,46 @@ import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
-const ProjectCard = ({
-    index,
-    name,
-    description,
-    tags,
-    image,
-    source_code_link,
-}) => {
+const ProjectCard = ({ name, description, tags, image, source_code_link }) => {
     return (
-        <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-            <Tilt
-                options={{
-                    max: 45,
-                    scale: 1,
-                    speed: 450,
-                }}
-                className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
-            >
-                <div className="relative w-full h-[230px]">
-                    <img
-                        src={image}
-                        alt="project_image"
-                        className="w-full h-full object-cover rounded-2xl"
-                    />
+        <div className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
+            <div className="relative w-full h-[230px]">
+                <img
+                    src={image}
+                    alt="project_image"
+                    className="w-full h-full object-cover rounded-2xl"
+                />
 
-                    <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-                        <div
-                            onClick={() =>
-                                window.open(source_code_link, "_blank")
-                            }
-                            className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-                        >
-                            <img
-                                src={github}
-                                alt="source code"
-                                className="w-1/2 h-1/2 object-contain"
-                            />
-                        </div>
+                <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+                    <div
+                        onClick={() => window.open(source_code_link, "_blank")}
+                        className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+                    >
+                        <img
+                            src={github}
+                            alt="source code"
+                            className="w-1/2 h-1/2 object-contain"
+                        />
                     </div>
                 </div>
+            </div>
 
-                <div className="mt-5">
-                    <h3 className="text-white font-bold text-[24px]">{name}</h3>
-                    <p className="mt-2 text-secondary text-[14px]">
-                        {description}
+            <div className="mt-5">
+                <h3 className="text-white font-bold text-[24px]">{name}</h3>
+                <p className="mt-2 text-secondary text-[14px]">{description}</p>
+            </div>
+
+            <div className="mt-4 flex flex-wrap gap-2">
+                {tags.map((tag) => (
+                    <p
+                        key={`${name}-${tag.name}`}
+                        className={`text-[14px] ${tag.color}`}
+                    >
+                        #{tag.name}
                     </p>
-                </div>
-
-                <div className="mt-4 flex flex-wrap gap-2">
-                    {tags.map((tag) => (
-                        <p
-                            key={`${name}-${tag.name}`}
-                            className={`text-[14px] ${tag.color}`}
-                        >
-                            #{tag.name}
-                        </p>
-                    ))}
-                </div>
-            </Tilt>
-        </motion.div>
+                ))}
+            </div>
+        </div>
     );
 };
 
@@ -92,13 +72,9 @@ const Works = () => {
                 </motion.p>
             </div>
 
-            <div className="mt-20 flex flex-wrap gap-7">
+            <div className="mt-20 flex flex-wrap justify-center gap-7">
                 {projects.map((project, index) => (
-                    <ProjectCard
-                        key={`project-${index}`}
-                        index={index}
-                        {...project}
-                    />
+                    <ProjectCard key={`project-${index}`} {...project} />
                 ))}
             </div>
         </>
