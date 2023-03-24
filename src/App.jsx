f7f1893ad59c3c31e22a85be73c9dec7a4aa0a6a
Hamza <hamza.ben.abdallah02@gmail.com>
2023-03-24 01:11:28 -0400

    Update the components order

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,9 @@ const App = () => {
                     <Hero />
                 </div>
                 <About />
-                <Experience />
-                <Tech />
                 <Works />
+                <Tech />
+                <Experience />
                 <div className="relative z-0">
                     <Contact />
                     <StarsCanvas />
