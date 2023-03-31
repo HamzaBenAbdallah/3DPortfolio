b370a884c45486c242519414a5fe45c0baf6dd79
Hamza <hamza.ben.abdallah02@gmail.com>
2023-03-31 12:30:40 -0400

    Fix env import

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,5 @@ import App from "./App";
 import "./index.css";
 import ReactGA from "react-ga4";
 
-ReactGA.initialize(VITE_APP_TRACKING_ID);
+ReactGA.initialize(import.meta.env.VITE_APP_TRACKING_ID);
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
