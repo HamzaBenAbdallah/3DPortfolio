8de1a859429dd5f2752dca3044306f4451127c21
Hamza <A%5&&jMFOBsNoyRK5Y1^>
2024-12-01 12:28:14 -0500

    center

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,10 @@ import { styles } from "../styles";
 
 const Hero = () => {
     return (
-        <section className="relative w-full h-screen mx-auto">
+        <section className="h-screen flex items-center justify-center">
             <div
-                className={`${styles.paddingX} absolute inset-0 top-[350px] max-w-5xl mx-auto flex flex-row items-start gap-10`}
-            >
+                className={`flex flex-row items-center gap-5`}
+            >    
                 <div className="flex flex-col justify-center items-center mt-5">
                     <div className="w-5 h-5 rounded-full bg-[#915eff]" />
                     <div className="w-1 sm:h-50 h-40 violet-gradient" />
